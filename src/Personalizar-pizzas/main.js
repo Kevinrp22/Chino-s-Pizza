531e77b107aca4f4c678e2569465dd145ff13d7c
Kevin <kemalan@hotmail.es>
2020-01-31 01:05:51 +0100

    +1 navbar totalmente conseguido
    barra hamburguesa fixed

--- src/Personalizar-pizzas/main.js
+++ src/Personalizar-pizzas/main.js
@@ -1,55 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /* MENU SIDEBAR */
-    $("#boton-menu").click(function(){
+    $("#boton-menu").click(function () {
         $("#sidebar").toggleClass("active");
     });
 
     /* INGREDIENTES */
-    $("#btn-añadir-carrito").click(function(){
-        if(!$("input[name='tamaño[]'").is(":checked")){
+    $("#btn-añadir-carrito").click(function (e) {
+        e.preventDefault()
+        if (!$("input[name='tamaño[]'").is(":checked")) {
             /* SI NO HA ELEGIDO UN TAMAÑO ENTONCES */
             alert("Seleciona un tamaño de pizza!");
             return false;
         }
-        if(!$("input[name='masa[]'").is(":checked")){
+        if (!$("input[name='masa[]'").is(":checked")) {
             /* SI NO HA ELEGIDO UN TIPO DE MASA ENTONCES */
             alert("Seleciona un tipo de masa!");
             return false;
         }
-        if(!$("input[name='ingredientes[]'").is(":checked")){
+        if (!$("input[name='ingredientes[]'").is(":checked")) {
             /* SI NO HA ELEGIDO UN INGREDIENTE ENTONCES */
             alert("Seleciona al menos 1 ingrediente!");
             return false;
         }
+        function RecorrerInputs() {
+            valor = 0;
+            totalIngredientes = 0
+            tamaño = 0
+            masa = ""
+            $("input[name='tamaño[]'").each(function () {
+                if (this.checked) {
+                    tamaño = $(this).val();
+                    totalTamaño = parseFloat(tamaño);
 
-        function RecorrerRadios(){
-            $("input[name='tamaño[]'").each(function(index){
-                if(this.checked){
-                    tamaño=$(this).val();
-                    console.log(tamaño)
                 }
-            }) 
-            $("input[name='masa[]'").each(function(index){
-                if(this.checked){
-                    masa=$(this).val();
-                    console.log(masa)
+            })
+            $("input[name='masa[]'").each(function () {
+                if (this.checked) {
+                    masa = $(this).val();
+
                 }
-            }) 
-            valor=0;
-            total=0
-            $("input[name='ingredientes[]'").each(function(index){
-                if(this.checked){
-                    if(isNaN(parseFloat($(this).val())))
-                    total+=0;
+            })
+
+            $("input[name='ingredientes[]'").each(function () {
+                if (this.checked) {
+                    var cantidad = ($(this).val());
+                    totalIngredientes += parseFloat(cantidad);
                 }
-                else{
-                    total+=parseFloat($(this.val()))
-                    console.log(total)
-                }
-            }) 
+            })
+
+            var totalTodo = parseFloat(totalTamaño + totalIngredientes).toFixed(2);
+            console.log("el total es " + totalTodo)
+            
+            $("#salida-precio").text(totalTodo + "€")
         }
-        RecorrerRadios()
 
+        RecorrerInputs()
+
+
+        
 
 
 
